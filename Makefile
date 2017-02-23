af7239506a754b11d02c296f1807885bbb95de87
Artem Melnykov <artem.melnikov@ignar.name>
2017-02-23 11:55:30 +0200

    Add install.sh file

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 all:
 	mkdir -p ~/bin
 	cp bin/* ~/bin/
-	echo 'set -gx PATH ~/bin $PATH' > ~/.config/fish/config.fish
+
---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+set -euf
+
+git clone https://github.com/ignar/git-plugins.git
+cd git-plugins
+make
